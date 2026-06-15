c15c13ff3bdba06a5a2bddb790b760c7c52529a9
Uppari Praveen <upparipraveen2003@gmail.com>
2026-06-15 16:38:00 +0530

    map pollyfill

--- Pollyfil.js
+++ Pollyfil.js
@@ -15,3 +15,29 @@ Array.prototype.myFilter = function(callBack){
 
 let res = arr.myFilter((item,index)=> item % 2 == 0)
 console.log(res)
+
+
+
+let arr = [1, 1, 2];
+
+// 1. Recreate the custom map method
+Array.prototype.myMap = function(callBack) {
+    let resultArray = [];
+    
+    // 'this' refers to the array (arr) that called myMap
+    for (let i = 0; i < this.length; i++) {
+        // Pass item, index, and the whole array into the callback
+        let result = callBack(this[i], i, this);
+        resultArray.push(result);
+    }
+    
+    return resultArray;
+};
+
+// 2. Test it out
+let doubled = arr.myMap((item, index) => {
+    console.log(`Processing index ${index}: ${item} * 2`);
+    return item * 2;
+});
+
+console.log("Returned Array:", doubled);
