214e3874efe2ad8737005ad3118611a9a71ac9ff
Uppari Praveen <upparipraveen2003@gmail.com>
2026-07-07 11:43:10 +0530

    nested key finding

--- Arrays.js
+++ Arrays.js
@@ -15,3 +15,47 @@ const res = arr.reduce((acc, item) => {
   acc[item.id] = item;
   return acc;
 }, {});
+
+
+const obj = {
+  user: {
+    name: "Praveen",
+    address: {
+      city: "Hyderabad",
+      location: {
+        area: "Madhapur",
+        pincode: 500081
+      }
+    }
+  },
+  company: {
+    name: "TechCorp",
+    details: {
+      founded: 2010,
+      address: {
+        city: "Bangalore",
+        country: "India"
+      }
+    }
+  }
+};
+
+function fn(obj,target,path,res){
+    for (let [key, val] of Object.entries(obj)) {
+    // console.log(key, val);
+    if(key == target){
+        res.push(path + "." + key)
+    }
+    if(typeof(val) === 'object'){
+        
+        let result =  fn(val,target,path === "" ? path + key : path +"." + key ,res)
+       
+    }
+    
+           
+        
+    }
+     return res
+   
+}
+console.log(fn(obj,"city","",[]))
